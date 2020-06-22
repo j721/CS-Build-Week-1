c1118862bc0b46dab0857ed0158c338e3fdf78a2
Julia Vu <juliadvu@gmail.com>
2020-06-22 01:14:35 -0500

    more updates to grid component

--- game-of-life/src/components/grid.js
+++ game-of-life/src/components/grid.js
@@ -2,11 +2,25 @@ import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
 
-const numRows = 50;
-cons numCols = 50; 
+const rows = 40;
+cons cols = 60; 
 
 
 function Grid(){
+    //set up initial state for grid
+    const [grid, setGrid] = useState(()=>{
+        const newRows = []; //rows set to an empty array
+        //for loop where i=0 means cell is dead and i= 1 is alive
+        //want to initialize an array of zeros. Have callback function return 0 
+        for (let i = 0; i< rows; i ++){
+            rows.push(Array.from(Array(cols), ()=> 0))
+        }//Array.from method takes in two parameters. The length of the array and a mapping function that returns the value and a key
+    })
+        return rows; 
+
+    console.log(grid)
+
+
     return (
         <></>
     )
