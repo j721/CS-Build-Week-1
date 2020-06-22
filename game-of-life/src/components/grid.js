0ff766f95ea93f42c04ec8cf558be47ba9c3d4f8
Julia Vu <juliadvu@gmail.com>
2020-06-22 01:17:05 -0500

    moved return statement up

--- game-of-life/src/components/grid.js
+++ game-of-life/src/components/grid.js
@@ -7,16 +7,17 @@ cons cols = 60;
 
 
 function Grid(){
-    //set up initial state for grid
+    //initial state for grid
     const [grid, setGrid] = useState(()=>{
         const newRows = []; //rows set to an empty array
+
         //for loop where i=0 means cell is dead and i= 1 is alive
         //want to initialize an array of zeros. Have callback function return 0 
         for (let i = 0; i< rows; i ++){
             rows.push(Array.from(Array(cols), ()=> 0))
         }//Array.from method takes in two parameters. The length of the array and a mapping function that returns the value and a key
-    })
         return rows; 
+    })
 
     console.log(grid)
 
