1adc9bf47b451136c2c3a4a0d6baef6b24af49ed
Julia Vu <juliadvu@gmail.com>
2020-06-22 01:41:21 -0500

    grid is showing on localhost

--- game-of-life/src/components/grid.js
+++ game-of-life/src/components/grid.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 
 
-const rows = 40;
+const numberOfRows = 40;
 const cols = 60; 
 
 
@@ -13,9 +13,11 @@ function Grid(){
 
         //for loop where i=0 means cell is dead and i= 1 is alive
         //want to initialize an array of zeros. Have callback function return 0 
-        for (let i = 0; i< rows; i ++){
+        for (let i = 0; i< numberOfRows; i ++){
             gridRows.push(Array.from(Array(cols), ()=> 0))
-        }//Array.from method takes in two parameters. The length of the array and a mapping function that returns the value and a key
+        }
+        //Array.from method takes in two parameters: 
+        //length of the array and a mapping function that returns the value and a key
         return gridRows; 
     })
 
@@ -23,7 +25,16 @@ function Grid(){
 
 
     return (
-        <></>
+        <>          
+        {grid.map((rows, i)=>rows.map((col, j) =>
+            <div 
+            key ={`${i}-${j}`} //unique key defined with index of i(rows) and j (col)
+            style ={{width: 20,                       
+            height: 20, backgroundColor: grid [i][j] ? 'pink' : undefined, //if alive pink, else undefined
+            border: 'solid 1px black' }}>
+
+            </div>))}
+        </>
     )
 }
 
